e6774bd99dda869cd173f453676500e815732ae9
dasom222g <dasom228@gmail.com>
2023-09-24 19:17:39 +0900

    Add ui file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+// import UserInfo from './pages/UserInfo';
+// import Chat from './pages/Chat';
 function App() {
-  return <div className="text-red-500">Hello world</div>;
+  // logic
+
+  // view
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/user-info" element={<UserInfo />} /> */}
+      {/* <Route path="/partner-info" element={<PartnerInfo />} /> */}
+      {/* <Route path="/chat" element={<Chat />} /> */}
+    </Routes>
+  );
 }
 
 export default App;
